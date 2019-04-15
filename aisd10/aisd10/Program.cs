c542fb0ae4c4a0146ae061e45cda608a77828b1e
RustamjonUsmonov11-811 <43405405+RustamjonUsmonov@users.noreply.github.com>
2019-04-15 19:54:56 +0300

    Add files via upload

---
+++ aisd10/aisd10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aisd10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int L = int.Parse(Console.ReadLine());
+            String[] A = { "QWE", "ASD", "ZXC", "ASDA", "OLKJ", "ZXF" };
+
+            var query = from s in A
+                        where s.Length == L
+                        select s;
+
+            Console.WriteLine(query.Max());
+
+            Console.ReadKey();
+        }
+
+    }
